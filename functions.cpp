2cb1387d677a0c3b3cb532409256748365de0318
sieep-coding <nstamb6@gmail.com>
2024-11-16 11:01:03 -0500

    function with no args but return values

--- functions.cpp
+++ functions.cpp
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-void find_max(int, int);
+int find_max();
 
 int main()
 {
-    int x, y;
-    printf("Enter any 2 values: \n");
-    scanf("%d%d", &x, &y);
-    find_max(x,y);
+    int z;
+    z = find_max();
+    printf("Maximum value is : %d \n", z);
     return 0;
 }
 
-void find_max(int x, int y)
+int find_max()
 {
-    int max;
+    int x, y, max;
+    printf("Enter any 2 values: \n");
+    scanf("%d%d", &x, &y);
     if (x > y)
         max = x;
     else
         max = y;
-    printf("Maximum value is: %d\n", max);
+    return max;
 }
