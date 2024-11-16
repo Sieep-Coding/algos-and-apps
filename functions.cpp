1b74bd128bb7e7979bc6431482c0c50976b79d9f
sieep-coding <nstamb6@gmail.com>
2024-11-16 10:56:57 -0500

    writing function without return statement

--- functions.cpp
+++ functions.cpp
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int find_max(int, int);
+void find_max(int, int);
 
 int main()
 {
-    int x = 10, y = 7;
-    printf("Max value is: %d\n", find_max(x, y));
+    int x, y;
+    printf("Enter any 2 values: \n");
+    scanf("%d%d", &x, &y);
+    find_max(x,y);
     return 0;
 }
 
-int find_max(int x, int y)
+void find_max(int x, int y)
 {
     int max;
     if (x > y)
         max = x;
     else
         max = y;
-    return max;
+    printf("Maximum value is: %d\n", max);
 }
